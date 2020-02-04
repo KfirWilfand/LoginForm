fe81685f1236602cda56ae00f7af98810b4f4411
zdennisz <zdennisz@gmail.com>
2020-02-04 14:08:37 +0200

    send email via mailgun and recive via fake email

--- modules/handlers/sendEmailHandler.js
+++ modules/handlers/sendEmailHandler.js
@@ -2,10 +2,11 @@ var nodemailer = require("nodemailer");
 
 function sendEmail(mailOptions) {
   var transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.mailgun.org",
+    port: 587,
     auth: {
-      user: "mytestkfir@gmail.com",
-      pass: "KaKa100100"
+      user: "postmaster@sandboxa2c1ef23f88e4e5ba703d83c31cf051d.mailgun.org",
+      pass: "e8992f98ddb24c35cf142d4130522747-f8faf5ef-561bb444"
     }
   });
 
