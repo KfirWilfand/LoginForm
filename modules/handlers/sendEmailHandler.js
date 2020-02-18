0727d2db9b1121c96720c30dd1cec16139904fa8
Kfir Wilfand <kfir333@gmail.com>
2020-02-18 21:59:40 +0200

    Update sendEmailHandler.js

--- modules/handlers/sendEmailHandler.js
+++ modules/handlers/sendEmailHandler.js
@@ -2,11 +2,11 @@ var nodemailer = require("nodemailer");
 
 function sendEmail(mailOptions) {
   var transporter = nodemailer.createTransport({
-    host: "smtp.mailgun.org",
+    host: "out.walla.co.il",
     port: 587,
     auth: {
-      user: "postmaster@sandboxa2c1ef23f88e4e5ba703d83c31cf051d.mailgun.org",
-      pass: "e8992f98ddb24c35cf142d4130522747-f8faf5ef-561bb444"
+      user: "tempam@walla.co.il",
+      pass: "funfun100100"
     }
   });
 
